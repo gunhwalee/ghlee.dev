d67d73628c11ef144e88999e9ae90f3d5dddc7d7
gunhwalee <ghlee2588@gmail.com>
2023-08-11 14:36:17 +0900

    메인 페이지 제작

--- src/components/TheWelcome.vue
+++ src/components/TheWelcome.vue
@@ -1,86 +1,52 @@
 <script setup lang="ts">
-import WelcomeItem from "./WelcomeItem.vue"
-import DocumentationIcon from "./icons/IconDocumentation.vue"
-import ToolingIcon from "./icons/IconTooling.vue"
-import EcosystemIcon from "./icons/IconEcosystem.vue"
-import CommunityIcon from "./icons/IconCommunity.vue"
-import SupportIcon from "./icons/IconSupport.vue"
+import { RouterLink } from "vue-router";
+import WelcomeItem from "./WelcomeItem.vue";
+import ProjectIcon from "./icons/IconProject.vue";
+import GithubIcon from "./icons/IconGithub.vue";
+import NoteIcon from "./icons/IconNote.vue";
+import PhoneIcon from "./icons/IconPhone.vue";
 </script>
 
 <template>
   <WelcomeItem>
     <template #icon>
-      <DocumentationIcon />
+      <RouterLink to="/projects">
+        <ProjectIcon />
+      </RouterLink>
     </template>
-    <template #heading>Documentation</template>
+    <template #heading>Projects</template>
 
-    Vue’s
-    <a href="https://vuejs.org/" target="_blank" rel="noopener">official documentation</a>
-    provides you with all information you need to get started.
+    <a href="https://spycat.netlify.app/" target="_blank" rel="noopener">Spy Cat</a>
+    은 사용자의 서버에서 발생한 트래픽과 에러 정보를 모니터링해주는 웹 애플리케이션입니다.
   </WelcomeItem>
 
   <WelcomeItem>
     <template #icon>
-      <ToolingIcon />
+      <a href="https://github.com/gunhwalee">
+        <GithubIcon />
+      </a>
     </template>
-    <template #heading>Tooling</template>
+    <template #heading>Github</template>
 
-    This project is served and bundled with
-    <a href="https://vitejs.dev/guide/features.html" target="_blank" rel="noopener">Vite</a>. The
-    recommended IDE setup is
-    <a href="https://code.visualstudio.com/" target="_blank" rel="noopener">VSCode</a> +
-    <a href="https://github.com/johnsoncodehk/volar" target="_blank" rel="noopener">Volar</a>. If
-    you need to test your components and web pages, check out
-    <a href="https://www.cypress.io/" target="_blank" rel="noopener">Cypress</a> and
-    <a href="https://on.cypress.io/component" target="_blank">Cypress Component Testing</a>.
-
-    <br />
-
-    More instructions are available in <code>README.md</code>.
+    <a href="https://vitejs.dev/guide/features.html" target="_blank" rel="noopener">Reactree</a>
+    는 React 프로젝트의 컴포넌트 계층구조를 트리 형태로 시각화해주는 Electron 기반의 데스크톱 앱 애플리케이션입니다.
   </WelcomeItem>
 
   <WelcomeItem>
     <template #icon>
-      <EcosystemIcon />
+      <a href="https://instinctive-bay-8e6.notion.site/Study-b4a75b3242d9490c8dc4ff77597c3454?pvs=4">
+        <NoteIcon />
+      </a>
     </template>
-    <template #heading>Ecosystem</template>
-
-    Get official tools and libraries for your project:
-    <a href="https://pinia.vuejs.org/" target="_blank" rel="noopener">Pinia</a>,
-    <a href="https://router.vuejs.org/" target="_blank" rel="noopener">Vue Router</a>,
-    <a href="https://test-utils.vuejs.org/" target="_blank" rel="noopener">Vue Test Utils</a>, and
-    <a href="https://github.com/vuejs/devtools" target="_blank" rel="noopener">Vue Dev Tools</a>. If
-    you need more resources, we suggest paying
-    <a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">Awesome Vue</a>
-    a visit.
+    <template #heading>Study Note</template>
   </WelcomeItem>
 
   <WelcomeItem>
     <template #icon>
-      <CommunityIcon />
+      <RouterLink to="/contact">
+        <PhoneIcon />
+      </RouterLink>
     </template>
-    <template #heading>Community</template>
-
-    Got stuck? Ask your question on
-    <a href="https://chat.vuejs.org" target="_blank" rel="noopener">Vue Land</a>, our official
-    Discord server, or
-    <a href="https://stackoverflow.com/questions/tagged/vue.js" target="_blank" rel="noopener"
-      >StackOverflow</a
-    >. You should also subscribe to
-    <a href="https://news.vuejs.org" target="_blank" rel="noopener">our mailing list</a> and follow
-    the official
-    <a href="https://twitter.com/vuejs" target="_blank" rel="noopener">@vuejs</a>
-    twitter account for latest news in the Vue world.
-  </WelcomeItem>
-
-  <WelcomeItem>
-    <template #icon>
-      <SupportIcon />
-    </template>
-    <template #heading>Support Vue</template>
-
-    As an independent project, Vue relies on community backing for its sustainability. You can help
-    us by
-    <a href="https://vuejs.org/sponsor/" target="_blank" rel="noopener">becoming a sponsor</a>.
+    <template #heading>Contact</template>
   </WelcomeItem>
 </template>
--- src/components/WelcomeItem.vue
+++ src/components/WelcomeItem.vue
@@ -3,25 +3,19 @@
     <i>
       <slot name="icon"></slot>
     </i>
-    <div class="details">
+    <div class="title">
       <h3>
         <slot name="heading"></slot>
       </h3>
-      <slot></slot>
     </div>
   </div>
 </template>
 
 <style scoped>
 .item {
-  margin-top: 2rem;
+  margin-top: 3rem;
   display: flex;
   position: relative;
-}
-
-.details {
-  flex: 1;
-  margin-left: 1rem;
 }
 
 i {
@@ -29,22 +23,25 @@ i {
   place-items: center;
   place-content: center;
   width: 32px;
-  height: 32px;
-
   color: var(--color-text);
+}
+
+.title {
+  margin-left: 1rem;
+  display: flex;
+  align-items: center;
 }
 
 h3 {
   font-size: 1.2rem;
   font-weight: 500;
-  margin-bottom: 0.4rem;
   color: var(--color-heading);
 }
 
 @media (min-width: 1024px) {
   .item {
     margin-top: 0;
-    padding: 0.4rem 0 1rem calc(var(--section-gap) / 2);
+    padding: 2rem 0 2rem calc(var(--section-gap) / 2);
   }
 
   i {
