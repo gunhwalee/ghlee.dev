02a1a62d616826c4c7e8ce2fadc4b94d6e9df4cb
gunhwalee <ghlee2588@gmail.com>
2023-08-12 17:36:45 +0900

    contact 페이지 작성

---
+++ src/components/TheContact.vue
@@ -0,0 +1,33 @@
+<template>
+  <h1 class="title">Contact</h1>
+  <div class="container">
+    <p>010-4120-2953</p>
+  </div>
+  <div class="container">
+    <p>ghlee2588@gmail.com</p>
+  </div>
+</template>
+
+<style scoped>
+.title {
+  color: #f2f2f2;
+  text-align: center;
+  margin-top: 3rem;
+}
+
+.container {
+  text-align: center;
+  margin: 2rem 0;
+}
+
+@media (min-width: 1024px) {
+  .container {
+    text-align: left;
+  }
+
+  .title {
+    margin-top: 0;
+    text-align: left;
+  }
+}
+</style>
---
+++ src/views/ContactView.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts">
+import theContact from "../components/TheContact.vue"
+</script>
+
+<template>
+  <main>
+    <theContact />
+  </main>
+</template>
