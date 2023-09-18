5152cf6365bc5182aa8af8e5f47d19216815a72d
gunhwalee <ghlee2588@gmail.com>
2023-09-18 11:05:17 +0900

    프로젝트 내용 수정

--- src/components/TheProject.vue
+++ src/components/TheProject.vue
@@ -9,9 +9,10 @@ import ProjectItem from "./ProjectItem.vue";
 
     <p class="description">
       Algolendar는 알고리즘 문제를 푼 기록을 연동해서 관리하는 달력형
-      애플리케이션으로 현재 작업중입니다.
+      애플리케이션입니다. Google Calendar와 Leetcode를 연동해 스케줄을 관리하고
+      기록을 공유할 수 있습니다. 현재 작업 진행중입니다.
     </p>
-    <p>TypeScript, Next.js, zustand, react-query...</p>
+    <p>TypeScript, Next.js</p>
     <nav>
       <a href="https://github.com/gunhwalee/algolendar" target="_blank"
         >Github</a
@@ -24,7 +25,8 @@ import ProjectItem from "./ProjectItem.vue";
 
     <p class="description">
       Spy Cat은 사용자의 서버에서 발생한 트래픽과 에러 정보를 모니터링해주는 웹
-      애플리케이션입니다.
+      애플리케이션입니다. 사용자의 서버에서 발생한 트래픽 에러 정보를 차트UI로
+      확인할 수 있으며, 특정 에러에 대한 상세 정보를 제공합니다.
     </p>
     <p>React, Redux, styled-components, Node.js, Express, MongoDB</p>
     <nav>
@@ -39,7 +41,9 @@ import ProjectItem from "./ProjectItem.vue";
 
     <p class="description">
       Reactree는 React 프로젝트의 컴포넌트 계층구조를 트리 형태로 시각화해주는
-      Electron 기반의 데스크톱 앱 애플리케이션입니다.
+      Electron 기반의 데스크톱 앱 애플리케이션입니다. 사용자가 작성한 React
+      프로젝트의 계층 구조를 시각화해서 제공하며, 각 컴포넌트의 상세 정보와
+      코드뷰어를 함께 제공합니다.
     </p>
     <p>React, Redux, styled-components, Electron</p>
     <nav>
